30ba93abcedd304961fb07411cfc95e127ec72e4
Pattarong <25365wowman@gmail.com>
2022-01-09 23:34:15 +0700

    front+backend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { RegisterComponent } from './page/register/register.component';
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
